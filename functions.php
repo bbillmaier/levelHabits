48e24ac9d148fbff7b06c0ed9ff94218ba13de01
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-09 21:59:32 -0500

    adding user array

--- functions.php
+++ functions.php
@@ -10,18 +10,11 @@
 	include 'Habitica.php';
 	include 'asana.php';
 	include 'get_all_tasks.php';
+	include 'user_array.php';
 
 	$asana = new Asana(array(
 		'personalAccessToken' => '0/32637edc751201d2e9d970badd364297'
 	));
 
 	
-	
-
-
-
-	$userId = 'bc40eff9-e00a-4509-b539-2c4e8723a098';
-	$apiToken = '07d0acde-d952-458e-aad1-72ca50df05c2';
-
-	$habitica = new Habitica($userId, $apiToken);
 ?>
---
+++ user_array.php
@@ -0,0 +1,8 @@
+<?php
+
+	$userArray = array(
+		'228439276172045' => array( 'bc40eff9-e00a-4509-b539-2c4e8723a098', '07d0acde-d952-458e-aad1-72ca50df05c2' ),
+		'228443959134227' => array( 'bc40eff9-e00a-4509-b539-2c4e8723a098', '07d0acde-d952-458e-aad1-72ca50df05c2' )
+	);
+
+?>
