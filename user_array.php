9af3acf81bf4b171bbe7547cf1c43b7bdeab92a7
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-09 22:00:14 -0500

    adding mm to user array

--- user_array.php
+++ user_array.php
@@ -2,7 +2,7 @@
 
 	$userArray = array(
 		'228439276172045' => array( 'bc40eff9-e00a-4509-b539-2c4e8723a098', '07d0acde-d952-458e-aad1-72ca50df05c2' ),
-		'228443959134227' => array( 'bc40eff9-e00a-4509-b539-2c4e8723a098', '07d0acde-d952-458e-aad1-72ca50df05c2' )
+		'228443959134227' => array( 'c8df7513-0279-433a-947a-afee7c00ffee', '8e7cc041-b9ef-41a6-ac75-9d4c4926cd6f' )
 	);
 
 ?>
