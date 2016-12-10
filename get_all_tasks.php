b5e0d5fa8af6f51b1c529ffba6607b9d90f76484
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-09 21:46:52 -0500

    changes to get all tasks

--- get_all_tasks.php
+++ get_all_tasks.php
@@ -3,12 +3,35 @@
 
 	$asana->getProjects();
 
+	$tasksArray = array();
+
+
 	foreach ($asana->getData() as $project) {
 
-		printArr($project);
+		//printArr($project);
 
 		$asana->getProjectTasks($project->id);
 
+		$tasksArray[] = $project->id;
+		echo $project->id.'<br>';
+		$projectTasks = $asana->getData();
+
+
+		print_r($asana->getData());
+
+		$i = 0;
+		foreach ($projectTasks as $task) {
+			
+			$asana->getTask($task->id);
+
+			echo $task->id.'<br>';
+
+			$taskDetails = $asana->getData();
+
+			$tasksArray[$project->id][$i] = $taskDetails;
+
+			$i++;
+		}
 	};
 
 	if ($asana->hasError()) {
@@ -16,6 +39,5 @@
 	    return;
 	}
 
-	printArr($asana->getData());
-
+	printArr($tasksArray);
 ?>
