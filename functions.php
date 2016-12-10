d1e1c70836af33e50b5d0bc73f0d21134e992581
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-09 21:06:15 -0500

    Adding asana class

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
 	include 'Habitica.php';
-	
-	$userId = 'c8df7513-0279-433a-947a-afee7c00ffee';
-	$apiToken = '8e7cc041-b9ef-41a6-ac75-9d4c4926cd6f';
+	include 'asana.php';
+
+	$asana = new Asana(array(
+		'personalAccessToken' => 'GET_IT_FROM_ASANA'
+	));
+	$userId = 'bc40eff9-e00a-4509-b539-2c4e8723a098';
+	$apiToken = '07d0acde-d952-458e-aad1-72ca50df05c2';
 
 	$habitica = new Habitica($userId, $apiToken);
 	function printArr($array){
