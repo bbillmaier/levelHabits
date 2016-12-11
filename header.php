e98c1f53e0b56111daa68a9709ae02acd44428da
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-11 16:56:42 -0500

    added materialize and color pallete

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+	include 'functions.php';
+?>
+<html>
+	<head>
+		<title>Level Habits</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+		<!--Import Google Icon Font-->
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+		<link rel="stylesheet" href="styles.css">
+		<script src="general.js"></script>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+<body>
+
+<nav>
+	<div class="nav-wrapper">
+	  <a href="#" class="brand-logo">Level Habits</a>
+	  <ul id="nav-mobile" class="right hide-on-med-and-down">
+	    <li><a href="sass.html">Sass</a></li>
+	    <li><a href="badges.html">Components</a></li>
+	    <li><a href="collapsible.html">JavaScript</a></li>
+	  </ul>
+	</div>
+</nav>
