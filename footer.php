e82f18478ec70860582aeea55054b761a52480fa
Blake Billmaier <blakebillmaier@gmail.com>
2016-12-11 17:33:35 -0500

    css updates

--- footer.php
+++ footer.php
@@ -1,2 +1,29 @@
+
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Level Habits</h5>
+        <p class="grey-text text-lighten-4">This tool was built by Marino McGalla and Blake Billmaier</p>
+      </div>
+      <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Tools</h5>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="#!">Add Users</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">View Tasks</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">View Users</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Update Tasks</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    © 2014 Copyright Text
+    <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    </div>
+  </div>
+</footer>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
 	include 'header.php';
 ?>
+<main>
+<div class="row">
+	<div class="col s12 m6">
+	  <div class="card">
+	    <div class="card-content white-text">
+	      <span class="card-title">Import Asana Tasks</span>
+	      <p>This will get all of the new Asana tasks and add them to our Database so that they can be added to Habitica.</p>
+	    </div>
+	    <div class="card-action">
+	      <a href="#">IMPORT</a>
+	    </div>
+	  </div>
+	</div>
+	<div class="col s12 m6">
+	  <div class="card">
+	    <div class="card-content white-text">
+	      <span class="card-title">Export Tasks to Habitica</span>
+	      <p>This will export all new tasks to Habitica.</p>
+	    </div>
+	    <div class="card-action">
+	      <a href="#">Export</a>
+	    </div>
+	  </div>
+	</div>
+</div>
 
-
-
+</main>
 <?php
 	include 'footer.php';
 ?>
